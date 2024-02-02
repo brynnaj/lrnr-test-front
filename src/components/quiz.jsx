93407c0b741bc4611113c6dc2600c52f8c34fa92
brynnaj <brynnaj78@gmail.com>
2024-02-02 11:02:30 -0500

    accepted changes

---
+++ src/components/quiz.jsx
@@ -0,0 +1,65 @@
+// import React from 'react'
+import { useState, useEffect } from 'react'
+
+export default function Quiz() {
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+
+    const [grade, setGrade] = useState('')
+
+   
+
+    // console.log(localStorage)
+    const questions = JSON.parse(localStorage.getItem('content'))
+    const filteredQuestions = questions.split('~~')
+
+    const updateQuestion = (event) => {
+        event.preventDefault()
+        setGrade('')
+        if (currentQuestion < filteredQuestions.length - 1) {
+            setCurrentQuestion(currentQuestion + 1)
+        } else if (currentQuestion === filteredQuestions.length - 1) {
+            setCurrentQuestion(0)
+            // window.location.href = '/result'
+        }
+        document.querySelector('input').value = ''
+    }
+
+    const checkAnswer = (event) => {
+        event.preventDefault()
+        const question = filteredQuestions[currentQuestion]
+        const answer = document.querySelector('input').value
+        fetch(`http://localhost:6747/grade?question=${question}&answer=${answer}`, {
+	        method: 'POST',
+            }).then(res => res.json())
+            .then(data => {
+                console.log(data.content)
+                setGrade(data.content)
+                
+            })
+        
+    }
+
+
+
+  return (
+    <div>
+      {filteredQuestions.map((question, index) => {
+            if (index === currentQuestion) {
+                return <div key={`question-${index}`}>
+                    <p>{currentQuestion+1}/{filteredQuestions.length}</p>
+                    <h1>{question}</h1>
+                    </div>
+            }
+      })}
+
+      <input type="text" placeholder='Answer'/>
+      <button onClick={checkAnswer}>submit</button>
+
+     
+
+      {grade} 
+      {grade &&  <button onClick={updateQuestion}>Next</button>}
+
+    </div>
+  )
+}
