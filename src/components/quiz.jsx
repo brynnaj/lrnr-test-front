458cbbfa398e154c373105a66fbc15348c63b0de
silvaom4 <silvaaom@gmail.com>
2024-02-08 16:17:31 -0500

    added window.location.href to result page

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,66 +1,69 @@
 // import React from 'react'
-import { useState, useEffect } from 'react'
-import '../quiz.css'
+import { useState, useEffect } from "react";
+import "../quiz.css";
 
 export default function Quiz() {
-    const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
-    const [grade, setGrade] = useState('')
+  const [grade, setGrade] = useState("");
 
-   
+  // console.log(localStorage)
+  const questions = JSON.parse(localStorage.getItem("content"));
+  console.log(questions);
+  const filteredQuestions = questions.split("~~");
 
-    // console.log(localStorage)
-    const questions = JSON.parse(localStorage.getItem('content'))
-    const filteredQuestions = questions.split('~~')
-
-    const updateQuestion = (event) => {
-        event.preventDefault()
-        setGrade('')
-        if (currentQuestion < filteredQuestions.length - 1) {
-            setCurrentQuestion(currentQuestion + 1)
-        } else if (currentQuestion === filteredQuestions.length - 1) {
-            setCurrentQuestion(0)
-            // window.location.href = '/result'
-        }
-        document.querySelector('input').value = ''
+  const updateQuestion = (event) => {
+    event.preventDefault();
+    setGrade("");
+    if (currentQuestion < filteredQuestions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else if (currentQuestion === filteredQuestions.length - 1) {
+      setCurrentQuestion(0);
+      window.location.href = "/result";
     }
+    document.querySelector("input").value = "";
+  };
 
-    const checkAnswer = (event) => {
-        event.preventDefault()
-        const question = filteredQuestions[currentQuestion]
-        const answer = document.querySelector('input').value
-        fetch(`http://localhost:6747/grade?question=${question}&answer=${answer}`, {
-	        method: 'POST',
-            }).then(res => res.json())
-            .then(data => {
-                console.log(data.content)
-                setGrade(data.content)
-                
-            })
-        
-    }
-
-
+  const checkAnswer = (event) => {
+    event.preventDefault();
+    const question = filteredQuestions[currentQuestion];
+    const answer = document.querySelector("input").value;
+    fetch(`http://localhost:6747/grade?question=${question}&answer=${answer}`, {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.content);
+        setGrade(data.content);
+      });
+  };
 
   return (
-    <div className='quiz-box'>
+    <div className="quiz-box">
       {filteredQuestions.map((question, index) => {
-            if (index === currentQuestion) {
-                return <div key={`question-${index}`}>
-                    <p>{currentQuestion+1}/{filteredQuestions.length}</p>
-                    <h1>{question}</h1>
-                    </div>
-            }
+        if (index === currentQuestion) {
+          return (
+            <div key={`question-${index}`}>
+              <p>
+                {currentQuestion + 1}/{filteredQuestions.length}
+              </p>
+              <h1>{question}</h1>
+            </div>
+          );
+        }
       })}
 
-      <input type="text" placeholder='Answer'/>
-      <button class="text-light bg-primary" onClick={checkAnswer}>Submit</button>
+      <input type="text" placeholder="Answer" />
+      <button class="text-light bg-primary" onClick={checkAnswer}>
+        Submit
+      </button>
 
-     
-
-      {grade} 
-      {grade &&  <button class="text-light bg-primary" onClick={updateQuestion}>Next</button>}
-
+      {grade}
+      {grade && (
+        <button class="text-light bg-primary" onClick={updateQuestion}>
+          Next
+        </button>
+      )}
     </div>
-  )
+  );
 }
